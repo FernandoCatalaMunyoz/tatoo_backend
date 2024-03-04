2e27b9080e956913d2f773ba3be9529a0f796294
Fernando <fernandocatalamunyoz@gmail.com>
2024-03-04 17:29:16 +0100

    feeat: services seed added

--- src/database/seeders/seeders.ts
+++ src/database/seeders/seeders.ts
@@ -1,4 +1,5 @@
 import { Role } from "../../models/Role";
+import { Service } from "../../models/Service";
 import { User } from "../../models/User";
 import { AppDataSource } from "../db";
 
@@ -65,8 +66,50 @@ const userSeedDatabase = async () => {
     await AppDataSource.destroy();
   }
 };
+
+const servicesSeedDatabase = async () => {
+  try {
+    await AppDataSource.initialize();
+
+    const service = new Service();
+    service.servicesName = "Tatuajes personalizados";
+    service.description =
+      "Los clientes tendrán la libertad de seleccionar motivos y diseños únicos, personalizando completamente su experiencia de tatuaje de acuerdo a sus preferencias y gustos.";
+    await service.save();
+
+    const service2 = new Service();
+    service2.servicesName = "Tatuajes del catálogo";
+    service2.description =
+      "Ofrecemos la realización de tatuajes basados en diseños predefinidos en nuestro catálogo. Los clientes pueden elegir entre una variedad de opciones estilizadas y probadas.";
+    await service2.save();
+
+    const service3 = new Service();
+    service3.servicesName = "Restauración y rejuvenecimiento de trabajos";
+    service3.description =
+      "Nos especializamos en la restauración y rejuvenecimiento de tatuajes existentes. Nuestros expertos trabajan para mejorar y renovar tatuajes antiguos, devolviéndoles su vitalidad.";
+    await service3.save();
+
+    const service4 = new Service();
+    service4.servicesName = "Colocación de piercings y dilatadores:";
+    service4.description =
+      "Ofrecemos servicios profesionales para la colocación de piercings y dilatadores. Nuestro equipo garantiza procedimientos seguros y estilos variados para satisfacer las preferencias individuales de nuestros clientes.";
+    await service4.save();
+
+    const service5 = new Service();
+    service5.servicesName = "Venta de piercings y otros artículos:";
+    service5.description =
+      "Además de nuestros servicios de aplicación, ofrecemos una selección de piercings y otros artículos relacionados con el arte corporal. Los clientes pueden adquirir productos de calidad para complementar su estilo único.";
+    await service5.save();
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await AppDataSource.destroy();
+  }
+};
+
 const startSeeder = async () => {
   await roleSeedDatabase();
   await userSeedDatabase();
+  await servicesSeedDatabase();
 };
 startSeeder();
